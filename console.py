972e7601cac1d58ae74eadf6a1af1d911516b746
bashar-karoury <basharalkaroury@hotmail.com>
2024-02-11 15:53:25 +0400

    Import all classes in console

--- console.py
+++ console.py
@@ -7,6 +7,11 @@ import cmd
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
+from models.city import City
+from models.state import State
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 import re
 import json
 class HBNBCommand(cmd.Cmd):
