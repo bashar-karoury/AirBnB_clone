b7a8a73ba63f738e46df2a2eb9bd6506840877da
bashar-karoury <basharalkaroury@hotmail.com>
2024-02-11 22:45:01 +0400

    Add unit test for base_model

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+""" This module instantiates an object of class FileStorage
+"""
 import sys
 from models.engine.file_storage import FileStorage
 sys.path.insert(0, '../')  # Add the parent directory to the Python path
